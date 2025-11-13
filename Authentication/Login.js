2874d66ff67144b3fd2f0fbfa9ee21d2d4114cac
vidya <vidya@bharaths-MacBook-Pro.local>
2025-11-13 11:02:04 +0530

    app icon

--- Authentication/Login.js
+++ Authentication/Login.js
@@ -93,7 +93,7 @@ const Login = ({ navigation }) => {
         rememberMe: true,
       });
 
-      const userData = response.data.empData;
+      const userData = response.data.user;
 
       await AsyncStorage.setItem('userId', userData.userId.toString());
       await AsyncStorage.setItem('userName', userData.name);
